444ac9a8331a289b9ab361ae64299b9156cc4148
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2017-09-15 14:52:13 +0800

    Fix typo

--- share/bz/close.sh
+++ share/bz/close.sh
@@ -4,7 +4,7 @@ Usage: bz close [-c comment | -e] pr
        bz close -h
 
 Options:
-    -C    -- -c "Commited. Thanks!"
+    -C    -- -c "Committed. Thanks!"
     -F    -- -c "Committed with major changes. Thanks!"
     -M    -- -c "Committed with minor changes. Thanks!"
     -c    -- optional comment (default none)
